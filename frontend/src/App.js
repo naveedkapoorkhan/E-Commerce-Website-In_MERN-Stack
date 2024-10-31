33127f040527729ed36a01190b941368eacfac67
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2024-10-30 20:47:22 -0700

    add routing to navbar.jsx and app.js and navbar.css is updated by anchor tag decoration none

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div>
-     <Navbar/>
+      <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory/>}/>
+        <Route path='/womens' element={<ShopCategory/>}/>
+        <Route path='/kids' element={<ShopCategory/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path=':productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>        
+      </Routes>
+      
+      </Router>
+    
     </div>
   );
 }
--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Cart = () => {
   return (
     <div>
-      Cart
-    
+      Cart 
     </div>
   )
 }
