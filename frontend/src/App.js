981d2c5d65bafad6185e55dd5e7a0c597b726b7a
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2024-10-30 19:30:47 -0700

    Navbar is created and css is also added to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <p>hi</p>
+    <div>
+     <Navbar/>
     </div>
   );
 }
