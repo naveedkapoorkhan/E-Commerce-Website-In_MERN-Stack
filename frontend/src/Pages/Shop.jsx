62272daeb9c8cedd3d317faab3eae8a7ea7805c1
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2024-10-31 21:53:47 -0700

    new collection component is created and implemented and css is added and call it inside shop.js

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollection from '../Components/NewCollections/NewCollection'
+
 
 const Shop = () => {
   return (
@@ -9,6 +11,7 @@ const Shop = () => {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollection/>
 
     
     </div>
